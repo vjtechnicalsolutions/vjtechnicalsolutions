81344d2f06d64465eb11849042e39ca5a1529830
vjtech <satcomyoki@gmail.com>
2026-09-22 18:33:17 +0530

    Remove WhatsApp contact from website

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -4,7 +4,6 @@ import Lenis from "lenis";
 import { Toaster } from "@/components/ui/sonner";
 import Header from "./Header";
 import Footer from "./Footer";
-import WhatsAppButton from "./WhatsAppButton";
 
 export default function Layout() {
   const location = useLocation();
@@ -32,7 +31,6 @@ export default function Layout() {
       <Header />
       <main>{<Outlet />}</main>
       <Footer />
-      <WhatsAppButton />
       <Toaster />
     </div>
   );
